5bb2700b45dcce52f8b39b24d51b4d2b22261ea6
madhur <madhur-prashant.bhandari@students.mq.edu.au>
2022-11-05 16:36:16 +1100

    Added test for coverage

--- src/main/java/com/curioushead/api/HomeController.java
+++ src/main/java/com/curioushead/api/HomeController.java
@@ -4,19 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
-public class GreetingController {
+public class HomeController {
 
-    private final GreetingService service;
-
-    public GreetingController(GreetingService service) {
-        this.service = service;
-    }
-
-    @RequestMapping("/greeting")
+    @RequestMapping("/")
     public @ResponseBody String greeting() {
-        return service.greet();
+        return "Hello, World";
     }
 
 }
