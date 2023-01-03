4db4ac99470d868b88eac42a7b6b369937a50d08
madhur <madhur-prashant.bhandari@students.mq.edu.au>
2023-01-03 21:06:58 +1100

    Creating new pipeline

--- src/main/java/com/curioushead/api/GreetingService.java
+++ src/main/java/com/curioushead/api/GreetingService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
     public String greet(String madhur) {
-        return "Hello, Madhur & Richa!";
+        return "Hello, Madhur!";
     }
 }
